9e38073b0b7e6b991402ba58112d96605cb75c90
Unknown <vishal.yathish@gmail.com>
2018-03-27 19:10:34 -0700

    Brute Force Scanner

---
+++ src/exactohackerprogram/BruteForceScanner.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package exactohackerprogram;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author tcsfremont
+ */
+public class BruteForceScanner {
+    public static void main (String[] args) {
+        
+    }
+}
+/*
+//Username Brute F. Scanner:
+    Get User Input for a max. value limit
+    UserNameGenerator()
+        var 1(digits) - give all options for digits ("1,2,3,4,5,6,7...")
+                   Function uses this along with max. value to create a list of options fitting requirements
+                   return # of options
+        var 2(characters)
+                   Same as var 1 BUT use all 26 letters (NO SYMBOLS OR PUNCTUATION MARKS)
+        var 3(digits AND char)
+                   Same as var 2 BUT use all 26 letters + 10 digits
+*/
